ba5dfed991c312d160a066faed82de6f373ad644
pcgamer1 <saxenasarthak72@gmail.com>
2020-08-03 00:36:18 +0530

    Init Commit

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState, lazy } from "react"
 import { Row, Col } from "react-bootstrap"
 import { retrieveItems } from "../api"
 import { useSearch } from "../hooks/useSearch"
